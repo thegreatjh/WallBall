626d5080eb199aefd0ea45bc49d71b26d8a00776
thegreatjh <74930658+thegreatjh@users.noreply.github.com>
2021-04-09 23:58:55 -0600

    Add files via upload

---
+++ Sound.java
@@ -0,0 +1,17 @@
+package minitennis;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound
+{
+
+    public static final AudioClip BALL = Applet.newAudioClip(Sound.class.
+            getResource("ball.wav"));
+    public static final AudioClip GAMEOVER = Applet.newAudioClip(Sound.class.
+            getResource("gameover.wav"));
+    public static final AudioClip BACK = Applet.newAudioClip(Sound.class.
+            getResource("back.wav"));
+
+    
+}
